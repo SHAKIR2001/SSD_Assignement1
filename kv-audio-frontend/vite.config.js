21c31d9c9fbceaacfe75cdcda4f66fba0437b186
Chathumi21 <chathumiisurika0221@gmail.com>
2026-09-25 12:37:52 +0530

    fix(security): add X-Frame-Options header to prevent clickjacking

--- kv-audio-frontend/vite.config.js
+++ kv-audio-frontend/vite.config.js
@@ -3,16 +3,20 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
+//Fix Missing Anti-clickjacking Header
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  
   ],
 
-    server: {
+  server: {
     host: '0.0.0.0',
     allowedHosts: ['host.docker.internal'],
+
+    headers: {
+      'X-Frame-Options': 'DENY',
+    },
   },
 });
-
